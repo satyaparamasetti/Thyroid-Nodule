3fddeab64208c37f35d35544bf7db3b7a83ae3da
satya <satyaparamasetti@gmail.com>
2026-04-06 17:03:23 +0530

    Complete PWA conversion with Install App button and professional metadata

---
+++ Accounts/templates/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'thyroscan-cache-v2';
+const urlsToCache = [
+  '/',
+  '/accounts/login/',
+  '/accounts/register/',
+  '/static/icon-192.png',
+  '/static/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => caches.match('/'));
+    })
+  );
+});
